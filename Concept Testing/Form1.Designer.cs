bfceec8261b7c8d8db853d8b152088ac43346299
bazaam <bazaam1@gmail.com>
2015-12-29 01:55:42 -0800

    I changed a thing to test git commits

--- Concept Testing/Form1.Designer.cs
+++ Concept Testing/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Concept_Testing
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -33,22 +33,23 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(229, 98);
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(208, 118);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(110, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "My button";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Generate a Monster";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(532, 255);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Main";
+            this.Text = "Welcome";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
--- Concept Testing/Form1.cs
+++ Concept Testing/Form1.cs
@@ -10,16 +10,16 @@ using System.Windows.Forms;
 
 namespace Concept_Testing
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
